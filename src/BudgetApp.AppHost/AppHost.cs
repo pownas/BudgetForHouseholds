6ea6d3d64a11d8061dee9ceb9e689cbcdd7496cf
Jonas Arvidson <pownas@outlook.com>
2025-08-27 22:06:32 +0200

    Update the Aspire AppHost to work with db

--- src/BudgetApp.AppHost/AppHost.cs
+++ src/BudgetApp.AppHost/AppHost.cs
@@ -2,18 +2,12 @@ using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-//builder.AddProject<Projects.BudgetApp_Blazor>("budgetapp-blazor");
-
-
-//TODO : Add a proper database for production use
-
 // Sqlite database resource (file-based for dev)
-//var db = builder.AddSqlite("budgetdb", builder.Environment.IsDevelopment() ? "./data/budget.db" : "budget.db");
+var db = builder.AddSqlite("budgetdb", builder.Environment.IsDevelopment() ? "./data/budget.db" : "budget.db");
 
 // API project
-var api = builder.AddProject<Projects.BudgetApp_Api>("api");
-    //TODO: Enable when using a shared database              
-    // .WithReference(db);
+var api = builder.AddProject<Projects.BudgetApp_Api>("api")          
+                 .WithReference(db);
 
 // Blazor server project (web)
 var web = builder.AddProject<Projects.BudgetApp_Blazor>("web")
